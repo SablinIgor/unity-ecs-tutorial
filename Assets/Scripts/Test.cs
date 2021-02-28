b2dc7133db539d472437815d16756885c7fd6eaa
SablinIgor <sablin.igor@gmail.com>
2021-02-28 19:00:30 +0300

    Initial Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+//namespace ECSTutorial
+//{ 
+    /// <summary>
+    /// Тестовый класс!
+    /// </summary>
+    public class Test : MonoBehaviour
+    {
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        private void Start()
+        {
+            Debug.Log("Start!");
+        }
+
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        private void Update()
+        {
+            Debug.Log("Update!");
+        }
+
+        /// <summary>
+        /// Что-то делает
+        /// </summary>
+        public void NewFunc()
+        {
+            Debug.Log("WWW");
+        }
+        
+        /// <summary>
+        /// Update is called once per frame
+        /// </summary>
+        public void NewFunc2()
+        {
+            Debug.Log("Update!");
+        }
+    }   
+//}
+
